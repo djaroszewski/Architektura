b3cb1458c0b39f478a6cca1256ecd0183710ed12
Darek Jaroszewski <dark90@o2.pl>
2012-10-16 13:25:10 +0200

    lsity

---
+++ mpps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpps::Application.config.secret_token = '273162c2452a6ce712f9b54873def1755dca075fe14d77b6c29c2504a040738c5c05bff5bc8f0ec29789804cf51041febd4d3a5db1ff4d039946d6f3b47dce69'
---
+++ mpps/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mpps::Application.config.session_store :cookie_store, key: '_mpps_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mpps::Application.config.session_store :active_record_store
