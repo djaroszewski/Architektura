77af557c5b3c2eb496a7be0c7ec8e3daee5972ab
Darek Jaroszewski <dark90@o2.pl>
2013-01-15 13:07:05 +0100

    com

---
+++ fortunka/spec/factories/users.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    provider "MyString"
+    uid "MyString"
+    name "MyString"
+    oauth_token "MyString"
+    oauth_expires_at "2013-01-14 23:00:48"
+  end
+end
